f8082579a502564642a6935255f0501dbcd7b85f
Jacob Smith <jakesmith.seo@gmail.com>
2022-09-14 12:07:13 -0500

    fixed event delagation to work properly

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 
 ///////////////////////////////////////
 // Modal window
-
+// ELEMENTS //
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const navToggle = document.querySelector('.nav__toggle');
 const navLinks = document.querySelector('.nav__links');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
 
+// FUNCTIONS //
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -21,10 +23,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-// Open Modal
+// RUN Open Modal
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-// Close Modal
+// RUN Close Modal
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -66,71 +68,29 @@ header.append(message);
 
 // Delete Cookie Message
 const cookieBtn = document.querySelector('.btn--close--cookie');
-// console.log(cookieBtn);
 cookieBtn.addEventListener('click', () => {
   message.remove();
 });
 
-// Lecture
+// Page Navagation
+btnScrollTo.addEventListener('click', () => {
+  const section1 = document.querySelector('#section--1');
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.nav__links').addEventListener('click', e => {
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+    // Get href value and store it into id var
+    const id = e.target.getAttribute('href');
+    // Select that element id and scroll it into view
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    // Close Nav Bar On Mobile
+    navLinks.classList.remove('nav__links--toggle');
+  }
+});
+
+// LECTURES //
 //////////////////////////////////
 /////////////////////////////////
 ////////////////////////////////
-
-// console.log(document.documentElement);
-// console.log(document.head);
-// console.log(document.body);
-
-// console.log(document.querySelector('.header'));
-// const allSections = document.querySelectorAll('.section');
-// console.log(allSections);
-
-// console.log(document.getElementById('section--1'));
-// const allButtons = document.getElementsByTagName('button');
-// console.log(allButtons);
-
-// console.log(document.getElementsByClassName('btn'));
-
-// Creating and Inserting Elements
-// .insertAjacentHTML
-
-// const h1 = document.querySelector('h1');
-// h1.insertAdjacentHTML('beforeend', '<p>hello world</p>');
-
-// header.prepend(message);
-
-// header.append(message.cloneNode(true));
-
-// header.before(message);
-// header.after(message);
-
-// Styles
-
-message.style.boxShadow = `inset 0px 11px 3px -10px #CCC`;
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
-
-logo.alt = 'Beautiful minimalist logo';
-console.log(logo.alt);
-logo.setAttribute('company', 'Bankist');
-// console.log(logo.getAttribute('designer'));
-
-const linkTwitter = document.querySelector('.twitter-link');
-const navLink1 = document.querySelector('.nav__link');
-console.log(navLink1);
-console.log(navLink1.href);
-console.log(navLink1.getAttribute('href'));
-console.log(linkTwitter.href);
-console.log(linkTwitter.getAttribute('href'));
-
-// Data attributes
-console.log(logo.dataset.versionNumber);
-
-// Classes
-logo.classList.add('c');
-logo.classList.remove('c');
-logo.classList.toggle('c');
-logo.classList.contains('c');
-
-// document.body.style.background = '#222';
